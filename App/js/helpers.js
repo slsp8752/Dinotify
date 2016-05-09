cad4d88beaad05c7544e8f9fb0dce52db6425d47
slsp8752 <slsp8752@colorado.edu>
2016-05-09 16:00:18 -0600

    Started exponential backoff for getting MAC address

--- App/js/helpers.js
+++ App/js/helpers.js
@@ -5,6 +5,7 @@ function strip(html)
    return tmp.textContent || tmp.innerText || "";
 }
 
+//Get Request, used to get the ESP's MAC address
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
@@ -15,7 +16,26 @@ function httpGetAsync(theUrl, callback)
 			var reArray = MACre.exec(resp); 
 			if(reArray != null) var reResult = reArray[1]; 
             callback(reResult);
+			return true;
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
+}
+
+//Exponential backoff for the GET request, in case the ESP isn't connected/the user isn't connected to the ESP's access point
+function exponentialBackoff(toTry, max, delay, callback) {
+    var result = toTry();
+    if (result != undefined) {
+        callback(result);
+    } else {
+        if (max > 0) {
+            setTimeout(function() {
+                exponentialBackoff(toTry, --max, delay * 2, callback);
+            }, delay);
+
+        } else {
+             console.log('Max backoffs exceeded');   
+			 callback(false);
+        }
+    }
 }
